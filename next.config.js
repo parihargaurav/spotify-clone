2bb2614d5a6ddf17fae1bca945ffac7751493bbe
Gaurav <parihargaurav527@gmail.com>
2023-06-30 01:56:34 +0530

    final

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            "vpnhstufgxwdhkdosioc.supabase.co"]
+    }
+}
 
 module.exports = nextConfig
